061798400ea45e9bd9bed974b94458fe77916106
Daniil Akifev <akifev.ru@gmail.com>
2019-11-10 07:52:29 +0300

    solved

--- src/emitter.js
+++ src/emitter.js
@@ -3,13 +3,15 @@
 /**
  * Сделано дополнительное задание: реализованы методы several и through.
  */
-const isExtraTaskSolved = false;
+const isExtraTaskSolved = true;
 
 /**
  * Получение нового Emitter'а
  * @returns {Object}
  */
 function getEmitter() {
+  const events = new Map();
+
   return {
     /**
      * Подписка на событие
@@ -18,7 +20,13 @@ function getEmitter() {
      * @param {Function} handler
      */
     on: function(event, context, handler) {
-      console.info(event, context, handler);
+      if (events.has(event)) {
+        events.get(event).push({ context: context, handler: handler });
+      } else {
+        events.set(event, [{ context: context, handler: handler }]);
+      }
+
+      return this;
     },
 
     /**
@@ -27,7 +35,22 @@ function getEmitter() {
      * @param {Object} context
      */
     off: function(event, context) {
-      console.info(event, context);
+      if (!events.has(event)) {
+        return this;
+      }
+
+      for (const eventName of events.keys()) {
+        if (eventName === event || eventName.startsWith(event + '.')) {
+          events.set(
+            eventName,
+            events.get(eventName).filter(function(subscriber) {
+              return subscriber.context !== context;
+            })
+          );
+        }
+      }
+
+      return this;
     },
 
     /**
@@ -35,7 +58,26 @@ function getEmitter() {
      * @param {string} event
      */
     emit: function(event) {
-      console.info(event);
+      function emitEvent(e) {
+        if (!events.has(e)) {
+          return;
+        }
+
+        for (const { context, handler } of events.get(e)) {
+          handler.call(context);
+        }
+      }
+
+      emitEvent(event);
+      for (let i = event.length - 1; i >= 0; i--) {
+        if (event[i] !== '.') {
+          continue;
+        }
+        const underEvent = event.substr(0, i);
+        emitEvent(underEvent);
+      }
+
+      return this;
     },
 
     /**
@@ -46,7 +88,18 @@ function getEmitter() {
      * @param {number} times Сколько раз отправить уведомление
      */
     several: function(event, context, handler, times) {
-      console.info(event, context, handler, times);
+      if (times <= 0) {
+        return this.on(event, context, handler);
+      }
+
+      let timesToCall = times;
+
+      return this.on(event, context, function() {
+        if (timesToCall > 0) {
+          handler.call(context);
+          --timesToCall;
+        }
+      });
     },
 
     /**
@@ -57,7 +110,19 @@ function getEmitter() {
      * @param {number} frequency Как часто уведомлять
      */
     through: function(event, context, handler, frequency) {
-      console.info(event, context, handler, frequency);
+      if (frequency <= 0) {
+        return this.on(event, context, handler);
+      }
+
+      let timesToSkip = 0;
+
+      return this.on(event, context, function() {
+        if (timesToSkip === 0) {
+          handler.call(context);
+          timesToSkip = frequency;
+        }
+        --timesToSkip;
+      });
     }
   };
 }
